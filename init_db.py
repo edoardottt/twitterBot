361db2493f116ca86b47b0c6ab8bcbce644867d6
gilfoyle97 <35783570+edoardottt@users.noreply.github.com>
2019-07-12 15:55:10 +0200

    Add files via upload
    fixed some errors

--- init_db.py
+++ init_db.py
@@ -23,12 +23,12 @@ def create_table(conn, create_table_sql):
         print(e)
 
 if db_is_new:
-    sql_create_users_table = """ CREATE TABLE users (
+    sql_create_users_table = """ CREATE TABLE IF NOT EXISTS users (
                                         username text PRIMARY KEY,
                                         password text NOT NULL
                                     ); """
  
-    sql_create_analytics_table = """CREATE TABLE analytics (
+    sql_create_analytics_table = """CREATE TABLE IF NOT EXISTS analytics (
                                     username text NOT NULL,
                                     date date NOT NULL,
                                     likes integer NOT NULL,
