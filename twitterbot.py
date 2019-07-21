1a5d719185d292ed813d1f2231923595bb4d960d
gilfoyle97 <35783570+edoardottt@users.noreply.github.com>
2019-07-21 11:04:54 +0200

    Add files via upload

--- twitterbot.py
+++ twitterbot.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Sun Jul  7 10:12:13 2019
 
 @author: edoardottt
 
-version = 1.3.2
+version = 1.3.3
 
 
 This is the main file.
@@ -13,27 +12,32 @@ This file must be execute from the command line because It have to read the inpu
 It searches for tweet links found in your personal Feed/via search input field.
 It can: Put likes on tweets and retweets them.
 It defines a class called TwitterBot.
-TwitterBot elements are built with 6 elements: username, password,likes,retweets,hashtags,followers.
+TwitterBot elements are built with 6 elements: username, password, likes ,retweets, keywords, followers.
 
 
 This file is under MIT License.
 
 
 """
-print('https://www.edoardoottavianelli.it')
+
+
 #VARIABLES TO CHANGE-----------------------------
-email_email = ''
-email_password = ''
+email_email = '' # user's username
+email_password = '' # user's password
 password_flag = False   # True if the password has been entered
-hashtags = []   # the list of words you want to search in the input field
-connection = 2  # time to wait
-hashtag_flag = False    # True if the -h option has been entered
+keywords = []   # the list of words you want to search in the input field
+connection = 3  # time to wait the website load the content completely
+keywords_flag = False    # True if the -k option has been entered
 stat_flag = False   # True if the -s option has been entered
 my_flag = False     # True if the -m option has been entered
-limit = 50000   # Limit of the links crawled 
-#-----------------------------------------------
+info_flag = False   # True if the -i option has been entered
+help_flag = False   # True if the -h option has been entered
+limit = 50000   # Limit of the links crawled
+
+#required libraries-----------------------------------------------
 import time
 import getopt
+import socket
 import getpass
 import random
 import datetime
@@ -42,40 +46,69 @@ import add_result
 import analyze_stat
 import sys,usage
 
+#write log into log file
+def write_log(ex):
+    f = open("twitterBot_log.txt",'a+')
+    f.write('-----------'+str(datetime.datetime.now())+'----------\n')
+    f.write(str(ex)+'\r\n')
+    f.close()
+
 try:
     from selenium import webdriver
     from selenium.webdriver.common.keys import Keys
 except Exception as ex:
+    write_log(ex)
     usage.print_usage(3)
-# all the libraries It needs
 
+# options input  
 try:
-    options,remainder =getopt.getopt(sys.argv[1:], 'u:h:sm',['username','hashtags','stat','mine'])  # all the options allowed
+    options,remainder =getopt.getopt(sys.argv[1:], 'u:k:smih',['username','keywords','stat','mine','info','help'])  # all the options allowed
     for opt, arg in options:
         if opt in ('-u','--username'):
             email_email = arg
-        elif opt in ('-h','--hashtags'):
+        elif opt in ('-k','--keywords'):
             try:
-                hashtag_flag = True
-                hashtags = arg.split(',')   # if more than one hashtag has been entered, split them and put into hashtags
+                keywords_flag = True
+                keywords = arg.split(',')   # if more than one keyword have been entered, split them and put into keywords
             except Exception as ex:
+                write_log(ex)
                 usage.print_usage(0)
         elif opt in ('-s','--stat'):
             stat_flag = True
         elif opt in ('-m','--mine'):
             my_flag = True
+        elif opt in ('-i','--info'):
+            info_flag = True
+        elif opt in ('-h','--help'):
+            help_flag = True
 except Exception as ex:
+    write_log(ex)
     usage.print_usage(0)
 
+#check internet connection status
+def internet_check(host="8.8.8.8", port=53):
+    """
+    Host: 8.8.8.8 (google-public-dns-a.google.com)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
+    try:
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except Exception as ex:
+        write_log(ex)
+        return False
+
+# twitterBot class definition
 class TwitterBot:
     
-    def __init__(self, username, password,likes,retweets,hashtags,followers):
+    def __init__(self, username, password,likes,retweets,keywords,followers):
         self.username = username
         self.password = password
         self.likes = 0
         self.retweets = 0
         self.followers = 0
-        self.hashtags = hashtags
+        self.keywords = keywords
         self.links = []
         self.bot = webdriver.Firefox()
         
@@ -110,6 +143,7 @@ class TwitterBot:
             try:
                 auth_flag = bot.find_element_by_css_selector("h1.Icon.Icon--bird.bird-topbar-etched") # auth_flag = get the bird icon on top of the page if the authentication is Okay
             except Exception as ex:
+                write_log(ex)
                 usage.print_usage(1)
             if (auth_flag != None):     # if the user is authorized
                 follower_elem = bot.find_element_by_css_selector('li.ProfileCardStats-stat:nth-child(3) > a:nth-child(1) > span:nth-child(2)')
@@ -120,14 +154,15 @@ class TwitterBot:
                 #bot.find_element_by_css_selector('a.r-1habvwh:nth-child(1) > div:nth-child(1)').click()
                 return True
         except Exception as ex:
+            write_log(ex)
             usage.print_usage(2)
     
     # add tweets links from search input field by typing the hashtags entered
     def add_links(self):
-        print('adding links...')
+        print('Collecting links...')
         bot = self.bot
-        for hashtag in self.hashtags:
-            bot.get('https://twitter.com/search?q=' + hashtag + '&src=typd')    # search the i-th hashtag
+        for keyword in self.keywords:
+            bot.get('https://twitter.com/search?q=' + keyword + '&src=typd')    # search the i-th hashtag
             time.sleep(connection)
             bot.execute_script('window.scroll(0,document.body.scrollHeight)')   # scroll the page
             time.sleep(connection)
@@ -138,7 +173,7 @@ class TwitterBot:
         
     #add tweets links from the personal feed    
     def add_links_my_home(self):
-        print('adding links...')
+        print('Collecting links...')
         bot = self.bot
         for i in range(7):
             bot.execute_script('window.scroll(0,document.body.scrollHeight)')   #scroll the page
@@ -154,7 +189,8 @@ class TwitterBot:
         print('TwitterBot started at '+str(datetime.datetime.now())[:-7]+" !")
         for link in self.links:
             if((not link is None) and (self.likes<limit)):  # if the tweets reached don't overcome the limit
-                self.bot.get('https://twitter.com'+link)    # get the tweet page 
+                self.bot.get('https://twitter.com'+link)    # get the tweet page
+                time.sleep(connection)
                 try:
                     self.bot.find_element_by_class_name('HeartAnimation').click()   # like
                     self.likes += 1
@@ -175,29 +211,43 @@ class TwitterBot:
         print('')
         print('Finished!')
 
-email_password = getpass.getpass('Insert password for ' +email_email +':') # password input via getpass
 
-# if password, username, and ( -m OR -h)
-if((email_email!='')and(email_password!='')and(not stat_flag)and((my_flag and (not hashtag_flag))or(hashtag_flag and (not my_flag)))):
-    edoBot = TwitterBot(email_email,email_password,0,0,hashtags,0) #create the bot
+# if -u and ( -m OR -k)
+if((email_email!='')and(not stat_flag)and((my_flag and (not keywords_flag))or(keywords_flag and (not my_flag)))):
+    
+    email_password = getpass.getpass('Insert password for ' +email_email +':') # password input via getpass
+    edoBot = TwitterBot(email_email,email_password,0,0,keywords,0) #create the bot
+    network_status = internet_check()
+    if (not network_status):
+        usage.print_usage(6) # if the network connection isn't active
     authenticated = edoBot.login()  # login
     if(authenticated): 
         if(check_user.check_if_user_exists(edoBot.username,edoBot.password)):
             print('Welcome back, ' + edoBot.username + ' !')
         else:
             print('Logged in as '+edoBot.username+' !')
-        if(hashtag_flag and(not my_flag)):
+        if(keywords_flag and(not my_flag)):
             edoBot.add_links()
-        elif(my_flag and(not hashtag_flag)):
+        elif(my_flag and(not keywords_flag)):
             edoBot.add_links_my_home()
         else:
-            usage.print_usage(0)
+            usage.print_usage(8)
         edoBot.crawl() #here start the bot
         edoBot.close()
         timee = datetime.datetime.now()
         add_result.add_stat(edoBot.username,timee,edoBot.likes,edoBot.retweets,edoBot.followers)
-# if password, username and -s 
-elif((email_email!='')and(not password_flag)and(not hashtag_flag)and(stat_flag)and(not my_flag)):
+        
+# if -u and -s 
+elif((email_email!='')and(not keywords_flag)and stat_flag and(not my_flag) and(not info_flag) and(not help_flag)):
+    email_password = getpass.getpass('Insert password for ' +email_email +':') # password input via getpass
     analyze_stat.check_stat(email_email,email_password)
-else:
+    
+# if -i
+elif((email_email=='') and(not stat_flag)and(not my_flag)and(not keywords_flag)and(not help_flag)and(info_flag)):
+    usage.print_usage(7)
+    
+# if -h
+elif((email_email=='') and(not stat_flag)and(not my_flag)and(not keywords_flag)and help_flag and(not info_flag)):
     usage.print_usage(0)
+else:
+    usage.print_usage(8)
