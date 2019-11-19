e84b84da5724b3f57794e936600cf3cb6adce162
gilfoyle97 <edoardott@gmail.com>
2019-11-19 16:35:25 +0100

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -49,56 +48,4 @@ sql_create_analytics_table = """CREATE TABLE IF NOT EXISTS analytics (
 if conn is not None:
     create_table(conn, sql_create_users_table)
     create_table(conn, sql_create_analytics_table)
-=======
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul  8 13:41:22 2019
-
-@author: edoardottt
-
-Initialization of database.
-
-This file is under MIT License.
-
-
-"""
-
-
-import os
-import sqlite3
-
-db_filename = 'database.db'
-
-db_is_new = not os.path.exists(db_filename)
-
-conn = sqlite3.connect(db_filename) # connect to the database or create it
-
-
-# ctreate table with the sql code input
-def create_table(conn, create_table_sql):
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Exception as e:
-        print(e)
-
-sql_create_users_table = """ CREATE TABLE IF NOT EXISTS users (
-                                        username text PRIMARY KEY,
-                                        password text NOT NULL
-                                    ); """
- 
-sql_create_analytics_table = """CREATE TABLE IF NOT EXISTS analytics (
-                                    username text NOT NULL,
-                                    date date NOT NULL,
-                                    likes integer NOT NULL,
-                                    retweets integer NOT NULL,
-                                    followers integer NOT NULL,
-                                    PRIMARY KEY (username,date)
-                                    FOREIGN KEY (username) REFERENCES users (username)
-                                );"""
-if conn is not None:
-    create_table(conn, sql_create_users_table)
-    create_table(conn, sql_create_analytics_table)
->>>>>>> e76c079a5db9b6ec7492b60b456307c099c71369
 conn.close()
